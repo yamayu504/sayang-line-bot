af22eda00d75d4b2ee94a58d767dd0a189240a21
yamayu504 <yamayu.7349@gmail.com>
2019-01-18 14:31:44 +0900

    no reply

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -14,17 +14,20 @@ class WebhookController < ApplicationController
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
+
     unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+     error 400 do 'Bad Request' end
     end
+
     events = client.parse_events_from(body)
+
     events.each { |event|
       case event
       when Line::Bot::Event::Message
 	case event.type
 	when Line::Bot::Event::MessageType::Text
 	  message = {type: 'text',text: event.message['text']}
-	  client.reply_message(event['replyToken'], message)
+	  #client.reply_message(event['replyToken'], message)
         end
       end
     }
