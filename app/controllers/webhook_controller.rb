f25587348d2efb6a67fc1dbc770ffd9a7a206ff8
yamayu504 <yamayu.7349@gmail.com>
2019-01-18 15:47:42 +0900

    copy

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,15 +1,22 @@
-require 'line/bot'  #gem 'line-bot-api'の利用
+class LinebotController < ApplicationController
+  require 'line/bot'  # gem 'line-bot-api'
 
-class WebhookController < ApplicationController
-  protect_from_forgery :except => [:callback] #CSRF対策無効
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 
   def callback
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-
     unless client.validate_signature(body, signature)
-     error 400 do 'Bad Request' end
+      error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
@@ -17,23 +24,17 @@ class WebhookController < ApplicationController
     events.each { |event|
       case event
       when Line::Bot::Event::Message
-	case event.type
-	when Line::Bot::Event::MessageType::Text
-	  message = {type: 'text',text: event.message['text']}
-	  client.reply_message(event['replyToken'], message)
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
         end
       end
     }
 
-   head :ok
+    head :ok
   end
-  private 
-
-  def client
-   @client ||= Line::Bot::Client.new {|config|
-	config.channel_secret = ENV["085c6531912a544ac62338c8c5a88285"]
-	config.channel_token  = ENV["qWSAno3Y+jZmXKyRWJsTxiuX1JNonuGoCgBRbFnpC1YZxrvrnN5IXOzA1pgpBEMSzvvst7bRFeWVSGdSOMiKlVG+klvr2NeGpi2Kbxi6NbCH5fYURcWh6xQuWjrruffQ5wBQTq/kFR079axxRzhPewdB04t89/1O/w1cDnyilFU="]
-   }
-  end
-
 end
