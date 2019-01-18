c56d5d02b6bd42d64eedfd1647757f6189810bf4
yamayu504 <yamayu.7349@gmail.com>
2019-01-18 13:26:20 +0900

    delete space

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,12 +3,12 @@ class WebhookController < ApplicationController
   protect_from_forgery :except => [:callback] #CSRF対策無効
 
 
-　def client
+  def client
    @client ||= Line::Bot::Client.new {|config|
 	config.channel_secret = ENV["085c6531912a544ac62338c8c5a88285"]
 	config.channel_token  = ENV["qWSAno3Y+jZmXKyRWJsTxiuX1JNonuGoCgBRbFnpC1YZxrvrnN5IXOzA1pgpBEMSzvvst7bRFeWVSGdSOMiKlVG+klvr2NeGpi2Kbxi6NbCH5fYURcWh6xQuWjrruffQ5wBQTq/kFR079axxRzhPewdB04t89/1O/w1cDnyilFU="]
 	}
-   end
+  end
 
   def callback
     body = request.body.read
@@ -18,21 +18,20 @@ class WebhookController < ApplicationController
       error 400 do 'Bad Request' end
     end
     events = client.parse_events_from(body)
-    events.each { |event|	
+    events.each { |event|
       case event
       when Line::Bot::Event::Message
 	case event.type
 	when Line::Bot::Event::MessageType::Text
 	  message = {
 	    type: 'text',
-	    text: event.message['text'] 
+	    text: event.message['text']
 	  }
 	  client.reply_message(event['replyToken'], message)
+        end
       end
-   end
-   }
-	
+    }
+
    head :ok
   end
 end
-	
